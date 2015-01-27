57ba05692ea4f9d147f4197e8989fa17d184ea92
tonic <tonicbupt@gmail.com>
2015-01-27 15:31:11 +0800

    add nginx reload

--- resource/console.py
+++ resource/console.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python
-#coding:utf-8
+# coding:utf-8
 
 from __future__ import absolute_import
 
@@ -7,34 +6,34 @@ import os
 import click
 import logging
 
-from ext.common import get_root_config
+from ext.common import load_etcd_config, get_root_config
+
 from resource.influxdb import add_influxdb
 from resource.sentry import add_sentry
+from resource.nginx import nginx_reload
 
 logger = logging.getLogger(__name__)
 
-OUTPATH_FORMATTER = '/NBE/%s/resource-%s'
+ETCD_CONFIG_PATH = os.path.expanduser('~/.armin.yaml')
 
 @click.group()
-@click.argument('name')
-@click.option('--path', '-p', default='/NBE/.root/config', help='Path for root config')
-@click.option('--env', '-e', default='prod', help='Config for which environment')
+@click.option('--config-path', '-p', default='/NBE/.root/config', help='Path for root config in etcd')
 @click.pass_context
-def cli(ctx, name, path, env):
-    if not os.getenv('ETCD'):
-        logger.warn('Please Set Etcd in environment first')
+def cli(ctx, config_path):
+    if not os.path.isfile(ETCD_CONFIG_PATH):
+        logger.error('etcd config %s not exist' % ETCD_CONFIG_PATH)
         ctx.exit(-1)
 
-    etcd_config = tuple(map(lambda z:(z[0], int(z[1])), (x.split(":"),))[0] for x in os.getenv('ETCD').split(','))
-    ctx.obj['app_config_path'] = OUTPATH_FORMATTER % (name, env)
-    ctx.obj['app_name'] = name
-    ctx.obj['root_config'], ctx.obj['etcd'], ctx.obj['influxdb'], ctx.obj['mysql'] = get_root_config(path, etcd_config)
+    etcd_config = load_etcd_config(ETCD_CONFIG_PATH)
+    ctx.obj['etcd_config'] = etcd_config
+    ctx.obj['root_config'] = get_root_config(config_path, etcd_config)
     logger.info('NBE Resource Tool')
 
 
 commands = cli.command()
 commands(add_influxdb)
 commands(add_sentry)
+commands(nginx_reload)
 
 
 def main():
--- resource/sentry.py
+++ resource/sentry.py
@@ -4,26 +4,30 @@ import click
 import logging
 import yaml
 
-from ext.common import save
+from ext.common import save, get_etcd_client, APP_CONFIG_PATH_FORMATTER
 from ext.sentry import create_sentry
 
 logger = logging.getLogger(__name__)
 
 
-@click.option('--platform', '-p', default='python', help='language')
-@click.option('--namespace', '-n', default='public', help='namespace of gitlab')
+@click.argument('appname')
+@click.option('--platform', '-p', default='python', help='Programming Language')
+@click.option('--namespace', '-n', default='public', help='Namespace of Gitlab')
 @click.pass_context
-def add_sentry(ctx, platform, namespace):
-    etcd, app_config_path = ctx.obj['etcd'], ctx.obj['app_config_path']
+def add_sentry(ctx, appname, platform, namespace):
+    etcd = get_etcd_client(**ctx.obj['root_config']['etcd_config'])
+    app_config_path = APP_CONFIG_PATH_FORMATTER.format(appname=appname)
+
     try:
         config = yaml.load(etcd.read(app_config_path).value) or {}
     except:
-        logger.info('app %s config not exist' % ctx.obj['app_name'])
+        logger.info('app %s config not exist' % appname)
         ctx.exit(-1)
 
     if 'sentry_dsn' in config:
-        logger.info('app %s already has sentry' % ctx.obj['app_name'])
+        logger.info('app %s already has sentry' % appname)
         return
-    sentry = create_sentry(ctx.obj['root_config']['sentry_url'], namespace, platform, ctx.obj['app_name'])
+
+    sentry = create_sentry(ctx.obj['root_config']['sentry_url'], namespace, platform, appname)
     config['sentry_dsn'] = sentry['dsn']
     save(etcd, app_config_path, config)
